62da88621a7625c85a95bf5841060b80c9b0f3dc
dtmarius <git@dtmarius.com>
2020-07-14 19:59:21 +0200

    gateway-2 tiny refactoring

--- src/main/java/com/dtmarius/gateway/HeaderUtils.java
+++ src/main/java/com/dtmarius/gateway/HeaderUtils.java
@@ -1,13 +1,22 @@
 package com.dtmarius.gateway;
 
+import java.util.Collections;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class HeaderUtils {
 
-    private static Set<String> restrictedHeaders = Set.of("connection", "content-length", "expect", "host", "upgrade");
+    private static final Set<String> RESTRICTED_HEADERS_SET;
+
+    static {
+        TreeSet<String> treeSet = new TreeSet<>(String.CASE_INSENSITIVE_ORDER);
+        treeSet.addAll(Set.of("connection", "content-length", "date", "expect",
+                "from", "host", "upgrade", "via", "warning"));
+        RESTRICTED_HEADERS_SET = Collections.unmodifiableSet(treeSet);
+    }
 
     public static boolean isHeaderRestricted(String headerName) {
-        return restrictedHeaders.contains(headerName.toLowerCase());
+        return RESTRICTED_HEADERS_SET.contains(headerName.toLowerCase());
     }
 
 }
