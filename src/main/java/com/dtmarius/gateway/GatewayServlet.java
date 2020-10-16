ff0a3a6470ee62135579ed1fcf48e4732606a688
dtmarius <git@dtmarius.com>
2020-10-17 01:37:06 +0200

    refactoring

--- src/main/java/com/dtmarius/gateway/GatewayServlet.java
+++ src/main/java/com/dtmarius/gateway/GatewayServlet.java
@@ -7,8 +7,6 @@ package com.dtmarius.gateway;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
-import java.net.URISyntaxException;
-import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -31,6 +29,33 @@ import javax.servlet.http.HttpServletResponse;
  * status code 200 is returned.
  * 
  * Headers can be processed stream up and down by various servlet filters.
+ * 
+ * configuration example:
+ * 
+ * <pre>
+ * {@code 
+    <servlet>
+        <servlet-name>backend1-gateway</servlet-name>
+        <servlet-class>com.dtmarius.gateway.GatewayServlet</servlet-class>
+        <init-param>
+            <param-name>requestURLRegex</param-name>
+            <!-- ^(?<protocol>https?)://(?:localhost|127.0.0.1)(?::8080)?/gateway/backend1/(?<path>.*) -->
+            <param-value>^(?&lt;protocol&gt;https?)://(?:localhost|127.0.0.1)(?::8080)?/gateway/backend1/(?&lt;path&gt;.*)</param-value>
+        </init-param>
+        <init-param>
+            <param-name>targetURLTemplate</param-name>
+            <param-value>${protocol}://example.com/${path}</param-value>
+        </init-param>
+        <init-param>
+            <param-name>respondStatuscodeOnBackendErrorWithBodyContent</param-name>
+            <param-value>200</param-value>
+        </init-param>
+    </servlet>
+    <servlet-mapping>
+        <servlet-name>backend1-gateway</servlet-name>
+        <url-pattern>/backend1/*</url-pattern>
+    </servlet-mapping>
+ * </pre>
  */
 public class GatewayServlet extends HttpServlet {
 
@@ -38,13 +63,17 @@ public class GatewayServlet extends HttpServlet {
 
     private final Logger log = Logger.getLogger(GatewayServlet.class.getSimpleName());
 
+
     private String requestURLRegex;
     private String targetURLTemplate;
+    private int respondStatuscodeOnBackendErrorWithBodyContent;
 
     private Pattern pattern;
 
     @Override
     public void init() throws ServletException {
+        this.respondStatuscodeOnBackendErrorWithBodyContent = Integer
+                .parseInt(this.getInitParameter("respondStatuscodeOnBackendErrorWithBodyContent"));
         this.requestURLRegex = this.getInitParameter("requestURLRegex");
         this.targetURLTemplate = this.getInitParameter("targetURLTemplate");
         this.pattern = Pattern.compile(this.requestURLRegex);
@@ -64,35 +93,43 @@ public class GatewayServlet extends HttpServlet {
         return resolvedTargetURL;
     }
 
+    protected HttpResponse<byte[]> getDestinationResponse(HttpRequestDto requestDto)
+            throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpResponse.BodyHandler<byte[]> bodyHandler = HttpResponse.BodyHandlers.ofByteArray();
+        HttpRequest targetHttpReq = HttpRequestMapper.mapHttpRequestDtoToHttpRequest(requestDto);
+        log.info("Calling URL: " + targetHttpReq.uri().toString());
+        HttpResponse<byte[]> httpResponse = client.send(targetHttpReq, bodyHandler);
+        return httpResponse;
+    }
+
     @Override
     protected void service(final HttpServletRequest request, final HttpServletResponse response)
             throws ServletException, IOException {
 
-        HttpRequestDto requestDto = HttpRequestMapper.mapHttpServletRequestToHttpRequestDto(request);
-        requestDto.url = resolveDestinationURL(pattern, targetURLTemplate, requestDto.url);
-
-        final HttpClient client = HttpClient.newHttpClient();
-        final HttpResponse.BodyHandler<byte[]> bodyHandler = HttpResponse.BodyHandlers.ofByteArray();
         try {
-            HttpRequest targetHttpReq = HttpRequestMapper.mapHttpRequestDtoToHttpRequest(requestDto);
-            log.info("Calling URL: " + targetHttpReq.uri().toString());
-            HttpResponse<byte[]> httpResponse = client.send(targetHttpReq, bodyHandler);
 
-            httpResponse.headers().map().forEach((headerName, headerValueList) -> {
+            HttpRequestDto requestDto = HttpRequestMapper.mapHttpServletRequestToHttpRequestDto(request);
+            requestDto.url = resolveDestinationURL(pattern, targetURLTemplate, requestDto.url);
+
+            HttpResponse<byte[]> destinationResponse = getDestinationResponse(requestDto);
+
+            destinationResponse.headers().map().forEach((headerName, headerValueList) -> {
                 String headerValue = headerValueList.stream().collect(Collectors.joining(", "));
                 log.info("httpResponse to res: " + headerName + ": " + headerValue);
 
                 if (HeaderUtils.isHeaderRestricted(headerName)) {
                     return;
                 }
-                // TODO: encode headervalues containing octet strtings
+                // TODO: check rfc2047 (4) for headerValue encoding and encode if necessary
                 response.addHeader(headerName, headerValue);
             });
 
-            byte[] body = httpResponse.body();
+            byte[] body = destinationResponse.body();
 
-            String charset = detectBodyCharacterEncoding(httpResponse);
+            String charset = detectBodyCharacterEncoding(destinationResponse);
 
+            // TODO: rewrite urls in body
             
             /**
              * TODO: Possible Feature: If the origin client send an accept or accept-charset
@@ -102,16 +139,17 @@ public class GatewayServlet extends HttpServlet {
             response.setCharacterEncoding(charset);
             response.getOutputStream().write(body);
 
+
             int status = response.getStatus();
             if (body.length > 0 && isSuccessfulStatusCode(status) == false) {
-                response.setStatus(HttpServletResponse.SC_OK); // TODO: 200? or 207 or ?
+                response.setStatus(this.respondStatuscodeOnBackendErrorWithBodyContent);
             }
 
-        } catch (InterruptedException | URISyntaxException e) { // TODO: rethink error handling
+        } catch (Exception e) {
+            // TODO: log exception
             e.printStackTrace();
+            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
         }
-
-        // response.sendError(404, "application error"); // TODO error codes
     }
 
     private String detectBodyCharacterEncoding(HttpResponse<byte[]> httpResponse) {
